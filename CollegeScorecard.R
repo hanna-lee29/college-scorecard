3b3595d8e0ef01e362d7d6ced047a9366a87c677
Hanna Lee <hannalee@Hannas-iMac.lan>
2025-02-20 22:13:34 -0500

    code working fetching data from api; need to make for loop dynamic

---
+++ CollegeScorecard.R
@@ -0,0 +1,55 @@
+install.packages("httr")
+install.packages("jsonlite")
+
+
+library(httr)
+library(jsonlite)
+
+api_key <- Sys.getenv("API_KEY")
+base_url <- "https://api.data.gov/ed/collegescorecard/v1/schools?"
+
+vars <- paste("id",
+              "school.name",
+              "latest.student.size",
+              "latest.student.retention_rate.four_year.full_time_pooled",
+              "latest.student.retention_rate.lt_four_year.full_time_pooled",
+              "latest.student.retention_rate.four_year.part_time_pooled",
+              "latest.student.retention_rate.lt_four_year.part_time_pooled",
+              "latest.completion.completion_rate_4yr_150_white",
+              "latest.completion.completion_rate_4yr_150_black",
+              "latest.completion.completion_rate_4yr_150_hispanic",
+              "latest.completion.completion_rate_4yr_150_asian",
+              "latest.completion.completion_rate_4yr_150_aian",
+              "latest.completion.completion_rate_4yr_150_nhpi",
+              "latest.completion.completion_rate_4yr_150_2ormore",
+              "latest.completion.completion_rate_4yr_150_nonresident.alien",
+              "latest.completion.completion_rate_4yr_150_race.unknown",
+              "latest.completion.completion_rate_l4yr_150_white",
+              "latest.completion.completion_rate_l4yr_150_black",
+              "latest.completion.completion_rate_l4yr_150_hispanic",
+              "latest.completion.completion_rate_l4yr_150_asian",
+              "latest.completion.completion_rate_l4yr_150_aian",
+              "latest.completion.completion_rate_l4yr_150_nhpi",
+              "latest.completion.completion_rate_l4yr_150_2ormore",
+              "latest.completion.completion_rate_l4yr_150_nonresident.alien",
+              "latest.completion.completion_rate_l4yr_150_race.unknown",
+              "latest.completion.completion_rate_4yr_150nt_pooled",
+              "latest.completion.completion_rate_less_than_4yr_150nt_pooled",
+              sep = ",")
+
+all_data <- list()
+page <- 1 
+for (page in 1:1) 
+  {
+  url <- paste0(base_url, "api_key=", api_key, "&fields=", vars, "&page=", page, "&per_page=100")
+  response <- GET(url)
+  data <- fromJSON(content(response,"text",encoding="UTF-8"), flatten = TRUE)$results
+  # if (any(sapply(data$id,is.null))){
+  #   break
+  # }
+  all_data <- append(all_data, list(data))
+  page <- page + 1
+}
+
+final_df <- do.call(rbind, lapply(all_data, as.data.frame))
+print(final_df)
