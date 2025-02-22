de7167d9ca6bdb2b9fe7525dd2856cbd7db51872
Hanna Lee <hannalee@Hannas-iMac.lan>
2025-02-22 12:36:53 -0500

    Modified for loop to break when ID is empty () to prevent unnecessary API calls.

--- CollegeScorecard.R
+++ CollegeScorecard.R
@@ -1,11 +1,8 @@
-install.packages("httr")
-install.packages("jsonlite")
-
-
 library(httr)
 library(jsonlite)
 
-api_key <- Sys.getenv("API_KEY")
+# api_key <- Sys.getenv("API_KEY")
+api_key <- "qWqq0Sjyjs9sJkJgr3SPt7ic53nx7UdIOw9dZriM"
 base_url <- "https://api.data.gov/ed/collegescorecard/v1/schools?"
 
 vars <- paste("id",
@@ -37,19 +34,23 @@ vars <- paste("id",
               "latest.completion.completion_rate_less_than_4yr_150nt_pooled",
               sep = ",")
 
+
 all_data <- list()
-page <- 1 
-for (page in 1:70) 
+page <- 1
+for (page in 1:100)
   {
   url <- paste0(base_url, "api_key=", api_key, "&fields=", vars, "&page=", page, "&per_page=100")
   response <- GET(url)
   data <- fromJSON(content(response,"text",encoding="UTF-8"), flatten = TRUE)$results
-  # if (any(sapply(data$id,is.null))){
-  #   break
-  # }
+  if (any(is.null(data$id))){
+
+    break
+  }
+
   all_data <- append(all_data, list(data))
   page <- page + 1
 }
+
 
 final_df <- do.call(rbind, lapply(all_data, as.data.frame))
 print(final_df)
